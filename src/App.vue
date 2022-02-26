4bcd554da333eb19348f0f320f1841d4d4be96b4
Antoine Apollis <antoinea@theodo.fr>
2022-02-26 12:58:39 +0100

    style: use computed to get column count

--- src/App.vue
+++ src/App.vue
@@ -3,11 +3,16 @@
     <img alt="Theodo" class="logo" src="./assets/theodo.svg" />
     <img alt="Vue" class="logo" src="./assets/vue.svg" />
   </header>
-  <main><CellGrid column-count="30" row-count="10" /></main>
+  <main><CellGrid :column-count="columnCount" :row-count="rowCount" /></main>
 </template>
 
 <script lang="ts" setup>
+import { computed, ref } from "vue";
+
 import CellGrid from "@/components/CellGrid.vue";
+
+const rowCount = ref(10);
+const columnCount = computed(() => 3 * rowCount.value);
 </script>
 
 <style>
